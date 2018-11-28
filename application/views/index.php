b7a5054f1371055a8553baca9680fd731626d6d2
Claudio Sepúlveda <claudiosepulveda.ac@gmail.com>
2018-11-27 23:40:56 -0300

    [KPI] Básico canvasjs

--- application/views/index.php
+++ application/views/index.php
@@ -24,155 +24,12 @@
 
 <br>
     <div class="container">
-      <div class="row">
-        <div class="col s12 m12 l12">
-          <div class="card-panel green lighten-5">
-            <div class="row">
-
-              <div class="col s12 m6 l6">
-                <div class="card">
-                  <div class="card-image waves-effect waves-block waves-light">
-                    <!-- <img class="activator" src="images/office.jpg"> -->
-                    <!-- <div id="kpi1" style="height: 200px; width: 100%;"></div> -->
-                    <div class="row sin-margin-bottom">
-                      <div class="col s6 center">
-                        <div class="card-panel valign-wrapper" style="height: 200px">
-                          <h5 class="valign-wrapper">Meta:<br>Mayor o Igual al 80% de cumplimiento</h5>
-                        </div>
-                      </div>
-                      <div class="col s6 center">
-                        <div class="card-panel porc_kpi1" style="height: 200px">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4"><b>KPI N°1:</b></span>
-                    <p>Tiempo de espera para pasar a hidrocooler no puede ser mayor a 60 minutos</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col s12 m6 l6">
-                <div class="card">
-                  <div class="card-image waves-effect waves-block waves-light">
-                    <div class="row sin-margin-bottom">
-                      <div class="col s6 center">
-                        <div class="card-panel valign-wrapper" style="height: 200px">
-                          <h5 class="valign-wrapper">Meta:<br>Mayor o Igual al 80% de cumplimiento</h5>
-                        </div>
-                      </div>
-                      <div class="col s6 center">
-                        <div class="card-panel porc_kpi2" style="height: 200px">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4"><b>KPI N°2:</b></span>
-                    <p>Tiempo de espera para pasar a proceso</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col s12 m12 l12">
-          <div class="card-panel green lighten-5">
-            <div class="row">
-              <div class="col s12 m3 l3">
-                <div class="card">
-                  <div class="card-image waves-effect waves-block waves-light">
-                    <div id="kpi1" style="height: 200px; width: 100%;"></div>
-                  </div>
-                  <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4"><b>Sanitización Cloro Concentrado</b></span>
-                    <p>Control concentracion cloro en circuitos de agua debe manterse entre 50 y 80 ppm</p>
-                  </div>
-                  <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col s12 m3 l3">
-                <div class="card">
-                  <div class="card-image waves-effect waves-block waves-light">
-                    <div id="kpi1" style="height: 200px; width: 100%;"></div>
-                  </div>
-                  <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4"><b>KPI N°1:</b><i class="material-icons right">more_vert</i></span>
-                    <p>Tiempo de espera para pasar a hidrocooler no puede ser mayor a 60 minutos</p>
-                  </div>
-                  <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col s12 m3 l3">
-                <div class="card">
-                  <div class="card-image waves-effect waves-block waves-light">
-                    <div id="kpi1" style="height: 200px; width: 100%;"></div>
-                  </div>
-                  <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4"><b>KPI N°1:</b></span>
-                    <p>Tiempo de espera para pasar a hidrocooler no puede ser mayor a 60 minutos</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col s12 m3 l3">
-                <div class="card">
-                  <div class="card-image waves-effect waves-block waves-light">
-                    <div id="kpi1" style="height: 200px; width: 100%;"></div>
-                  </div>
-                  <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4"><b>KPI N°1:</b><i class="material-icons right">more_vert</i></span>
-                    <p>Tiempo de espera para pasar a hidrocooler no puede ser mayor a 60 minutos</p>
-                  </div>
-                  <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col s12 m3 l3">
-                <div class="card">
-                  <div class="card-image waves-effect waves-block waves-light">
-                    <div id="kpi1" style="height: 200px; width: 100%;"></div>
-                  </div>
-                  <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4"><b>KPI N°1:</b><i class="material-icons right">more_vert</i></span>
-                    <p>Tiempo de espera para pasar a hidrocooler no puede ser mayor a 60 minutos</p>
-                  </div>
-                  <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col s12 m3 l3">
-                <div class="card">
-                  <div class="card-image waves-effect waves-block waves-light">
-                    <div id="kpi1" style="height: 200px; width: 100%;"></div>
-                  </div>
-                  <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4"><b>KPI N°1:</b><i class="material-icons right">more_vert</i></span>
-                    <p>Tiempo de espera para pasar a hidrocooler no puede ser mayor a 60 minutos</p>
-                  </div>
-                  <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
 
 
 
-      <!-- <div class="row" id="rowKpi1">
+
+
+      <div class="row" id="rowKpi1">
         <div class="col s12 m12 l12">
           <div class="card darken-5">
             <div class="card-content">
@@ -210,7 +67,7 @@
             </div>
           </div>
         </div>
-      </div> -->
+      </div>
 
     </div>
 
